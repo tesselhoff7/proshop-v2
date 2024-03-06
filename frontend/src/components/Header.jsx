c0d6517312567a4a045801e837e1a67832a575c0
ronnie_park <tesselhoff7@gmail.com>
2024-03-06 21:05:25 +0900

    backend enviroment set up. WIP

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -29,3 +29,43 @@ export default function Header() {
     </header>
   );
 }
+
+//import { Navbar, Nav, Container } from "react-bootstrap";
+//import { FaShoppingCart, FaUser } from "react-icons/fa";
+//import { LinkContainer } from "react-router-bootstrap";
+//import logo from "..//assets/logo.png";
+
+//export default function Header() {
+//  return (
+//    <header>
+//      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+//        <Container>
+//          {/*<LinkContainer to="/">*/}
+//          <Navbar.Brand>
+//            <img src={logo} alt="logo" width="50" height="50" />
+//            ProShop
+//          </Navbar.Brand>
+//          {/*</LinkContainer>*/}
+
+//          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+//          <Navbar.Collapse id="basic-navbar-nav">
+//            <Nav className="ml-auto">
+//              {/*<LinkContainer to="/cart">*/}
+//              <Nav.Link>
+//                <FaShoppingCart /> Cart
+//              </Nav.Link>
+//              {/*</LinkContainer>*/}
+
+//              {/*<LinkContainer href="/login">*/}
+//              <Nav.Link>
+//                <FaUser /> Sign In
+//              </Nav.Link>
+//              {/*</LinkContainer>*/}
+//            </Nav>
+//          </Navbar.Collapse>
+//        </Container>
+//      </Navbar>
+//    </header>
+//  );
+//}
