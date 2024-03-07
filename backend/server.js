675aac1012fc88eab6920313e665582b3e125b07
ronnie_park <tesselhoff7@gmail.com>
2024-03-07 20:50:59 +0900

    Add env, logic to call data from backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/products.js";
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -14,7 +16,7 @@ app.get("/api/products", (req, res) => {
 });
 
 app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p.find === req.params.id);
+  const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import products from "../products";
 import { Row, Col, Image, ListGroup, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import axios from "axios";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
