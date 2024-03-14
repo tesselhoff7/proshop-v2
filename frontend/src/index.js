8a8ce6a000867f81ed0320096d87efbaa7631805
ronnie_park <tesselhoff7@gmail.com>
2024-03-15 00:29:40 +0900

    Redux Set up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import "./assets/styles/bootstrap.custom.css";
-import "./assets/styles/index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import store from "./store";
+
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,7 +29,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
