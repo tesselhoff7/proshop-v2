bd45b2ec6a556e0e468d116c884d5bc5ac5d72cc
ronnie_park <tesselhoff7@gmail.com>
2024-03-25 20:28:27 +0900

    user Controller and user Routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,98 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+//@desc Auth user & get token
+//@route POST /api/users/login
+//@access Public
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+
+  res.send("auth user");
+});
+
+//@desc Register new user
+//@route POST /api/users
+//@access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("register user");
+});
+
+//@desc Logout user / clear cookie
+//@route GET /api/users/logout
+//@access Private
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("logout user");
+});
+
+//@desc Get use profile
+//@route GET /api/users/profile
+//@access Public
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("get user profile");
+});
+
+//@desc Update use profile
+//@route PUT /api/users/profile
+//@access Public
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("get user profile");
+});
+
+//@desc Get users
+//@route PUT /api/users
+//@access Private/Admin
+
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get users");
+});
+
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("get user by id");
+});
+
+//@desc Delete user
+//@route DELTE /api/users/:id
+//@access Public
+
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete user");
+});
+
+//@desc Update user
+//@route PUT /api/users/:id
+//@access Private/Admin
+
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("get user by id");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+};
