22df89f566a7ac6091a441429ead10b442ac2f19
ronnie_park <tesselhoff7@gmail.com>
2024-03-25 17:09:01 +0900

    comment

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,6 @@
 //export const BASE_URL =
 //  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+// TO FIX
 
 export const BASE_URL = "";
 export const PRODUCTS_URL = "/api/products";
