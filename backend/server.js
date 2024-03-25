2e3001fa1ef40be1432a4f1d048ba781c3a59547
ronnie_park <tesselhoff7@gmail.com>
2024-03-25 20:28:15 +0900

    server and user Model

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+
 dotenv.config();
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
@@ -12,11 +14,15 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
