6489b0763e56687b90d8ce7522f0a8f23e14eead
ronnie_park <tesselhoff7@gmail.com>
2024-03-27 20:26:58 +0900

    Logout User Endpoint & Endcryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,7 +53,12 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Logged out succesfully" });
 });
 
 //@desc Get use profile
