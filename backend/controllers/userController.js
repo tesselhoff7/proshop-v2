0513890c5318c5a782c9335207bad9a96880f13c
ronnie_park <tesselhoff7@gmail.com>
2024-03-27 21:00:22 +0900

    Update Profile Endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -80,7 +80,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 //@access Public
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("get user profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 //@desc Update use profile
@@ -88,7 +100,30 @@ const getUserProfile = asyncHandler(async (req, res) => {
 //@access Public
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("get user profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    generateToken(res, updatedUser._id);
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 //@desc Get users
