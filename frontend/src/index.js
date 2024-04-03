5183b231f8b884b6185a279aa5abf0ad1e1d3058
ronnie_park <tesselhoff7@gmail.com>
2024-04-03 19:27:19 +0900

    Private Routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 
 import App from "./App";
+import PrivateRoute from "./components/PrivateRoute";
 import reportWebVitals from "./reportWebVitals";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
@@ -29,7 +30,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
