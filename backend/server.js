ecfe21cc04deee5663b49cbbf8b71669606dd6aa
ronnie_park <tesselhoff7@gmail.com>
2024-06-09 22:41:57 +0900

    Multer & Image Upload Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+//import products from "./data/products.js";
 import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -28,9 +30,14 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+const __dirname = path.resolve(); // current directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads"))); // make uploads folder static
 
 app.use(notFound);
 app.use(errorHandler);
