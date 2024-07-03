d2a2a580d9b296b8a3a6f60fc5b114a814d740ad
tesselhoff7 <tesselhoff7@gmail.com>
2024-07-03 09:48:39 +0900

    Create review Frontend

--- frontend/src/slices/productsApiSLice.js
+++ frontend/src/slices/productsApiSLice.js
@@ -43,6 +43,15 @@ export const productsApi = apiSlice.injectEndpoints({
         method: "DELETE",
       }),
     }),
+
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Product"],
+    }),
   }),
 });
 
@@ -53,4 +62,5 @@ export const {
   useUpdateProductMutation,
   useUploadProductImageMutation,
   useDeleteProductMutation,
+  useCreateReviewMutation,
 } = productsApi;
